8c5e720c15c4101d3ba12ac5db42770a6935c3d4
Julian Heinath <jheinath@grenkeleasing.com>
2021-06-23 14:17:10 +0200

    removed template code, added basic view rendering with styling

---
+++ GameBoard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLiveBlazorWASM
+{
+    public class GameBoard
+    {
+        public bool[,] Cells { get; }
+
+        private GameBoard(int size, bool[,] startingCells)
+        {
+            Cells = new bool[size, size];
+
+            for (var i = 0; i < Cells.GetLength(0); i++)
+            {
+                for (var j = 0; j < Cells.GetLength(1); j++)
+                {
+                    if (startingCells[i, j])
+                    {
+                        Cells[i, j] = true;
+                    }
+                    else
+                    {
+                        Cells[i, j] = false;
+                    }
+                }
+            }
+        }
+
+        public static GameBoard Create(int size, bool[,] startingCells)
+        {
+            return new GameBoard(size, startingCells);
+        }
+
+    }
+}
