a4b1567eba78f300363155c58c4cf743abc8f2c4
jheinath <julianheinath@gmail.com>
2022-12-05 21:33:07 +0100

    push gameboard darkmode

--- wwwroot/scripts.js
+++ wwwroot/scripts.js
@@ -1,6 +1,7 @@
 ﻿function toggleDarkMode() {
     const elements = document.querySelectorAll("[class*=bg-light]");
     if (elements.length > 0){
+        const boxes = document.querySelectorAll('[id*=box]');
         const elementsDark = document.querySelectorAll("[class*=text-dark]");
         elements.forEach(element => {
             element.classList.replace('bg-light', 'bg-dark')
@@ -8,14 +9,29 @@
         elementsDark.forEach(element => {
             element.classList.replace('text-dark', 'text-light')
         });
+        boxes.forEach(element => {
+            if (element.classList.contains('bg-white')){
+                element.classList.replace('bg-white', 'bg-black')
+            } else {
+                element.classList.replace('bg-black', 'bg-white')
+            }
+        })
     } else {
         const elementsLight = document.querySelectorAll("[class*=text-light]");
         const elementsDark = document.querySelectorAll("[class*=bg-dark]");
+        const boxes = document.querySelectorAll('[id*=box]');
         elementsDark.forEach(element => {
             element.classList.replace('bg-dark', 'bg-light')
         });
         elementsLight.forEach(element => {
             element.classList.replace('text-light', 'text-dark')
+        })
+        boxes.forEach(element => {
+            if (element.classList.contains('bg-white')){
+                element.classList.replace('bg-white', 'bg-black')
+            } else {
+                element.classList.replace('bg-black', 'bg-white')
+            }
         })
     }
     
