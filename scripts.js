2d5d154e16438da73c6513cfbbbef24a69b0bd70
jheinath <44709506+jheinath@users.noreply.github.com>
2022-12-05 20:14:54 +0000

    Deploying to gh-pages from  @ be3ef529dcb7dce13afe2d71797a306b2d3c3ec4 🚀

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
 ﻿function toggleDarkMode() {
-    console.log('test');
+    const elements = document.querySelectorAll("[class*=bg-light]");
+    if (elements.length > 0){
+        const elementsDark = document.querySelectorAll("[class*=text-dark]");
+        elements.forEach(element => {
+            element.classList.replace('bg-light', 'bg-dark')
+        });
+        elementsDark.forEach(element => {
+            element.classList.replace('text-dark', 'text-light')
+        });
+    } else {
+        const elementsLight = document.querySelectorAll("[class*=text-light]");
+        const elementsDark = document.querySelectorAll("[class*=bg-dark]");
+        elementsDark.forEach(element => {
+            element.classList.replace('bg-dark', 'bg-light')
+        });
+        elementsLight.forEach(element => {
+            element.classList.replace('text-light', 'text-dark')
+        })
+    }
+    
 }
