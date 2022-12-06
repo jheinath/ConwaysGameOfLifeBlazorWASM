427439f3b0c695b4a9bde6110b287718a0645681
jheinath <julianheinath@gmail.com>
2022-12-06 23:44:25 +0100

    remove dark mode from gameboard due to ui bugs

--- wwwroot/scripts.js
+++ wwwroot/scripts.js
@@ -1,7 +1,6 @@
 ﻿function toggleDarkMode() {
     const elements = document.querySelectorAll("[class*=bg-light]");
     if (elements.length > 0){
-        const boxes = document.querySelectorAll('[id*=box]');
         const elementsDark = document.querySelectorAll("[class*=text-dark]");
         elements.forEach(element => {
             element.classList.replace('bg-light', 'bg-dark')
@@ -9,29 +8,14 @@
         elementsDark.forEach(element => {
             element.classList.replace('text-dark', 'text-light')
         });
-        boxes.forEach(element => {
-            if (element.classList.contains('bg-white')){
-                element.classList.replace('bg-white', 'bg-black')
-            } else {
-                element.classList.replace('bg-black', 'bg-white')
-            }
-        })
     } else {
         const elementsLight = document.querySelectorAll("[class*=text-light]");
         const elementsDark = document.querySelectorAll("[class*=bg-dark]");
-        const boxes = document.querySelectorAll('[id*=box]');
         elementsDark.forEach(element => {
             element.classList.replace('bg-dark', 'bg-light')
         });
         elementsLight.forEach(element => {
             element.classList.replace('text-light', 'text-dark')
-        })
-        boxes.forEach(element => {
-            if (element.classList.contains('bg-white')){
-                element.classList.replace('bg-white', 'bg-black')
-            } else {
-                element.classList.replace('bg-black', 'bg-white')
-            }
         })
     }
     
